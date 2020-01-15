d94e1da7a8294e0f3fc1d31184bd60589d976985
merkuler <43567118+merkuler@users.noreply.github.com>
2020-01-15 07:56:47 +0300

    changes

--- application/controllers/AccountController.php
+++ application/controllers/AccountController.php
@@ -12,12 +12,12 @@ class AccountController extends Controller
   {
 
     $this->view->render('Вход');
-          
+
 
   }
 
   public  function registerAction()
-  {      
+  {
     $this->view->render('Регистрация');
   }
 
@@ -29,27 +29,42 @@ class AccountController extends Controller
   //  $vars = ['result' =>$enter];
   // $this->view->render('Регистрация',$vars);
 
-  
+
   $login = filter_var(trim($_POST['login']),FILTER_SANITIZE_STRING);
   $name = filter_var(trim($_POST['name']),FILTER_SANITIZE_STRING);
   $pass = filter_var(trim($_POST['pass']),FILTER_SANITIZE_STRING);
 
-  if(mb_strlen($login)<5 || mb_strlen($login) > 90){
+
+  if(mb_strlen($login)<3 || mb_strlen($login) > 90){
     echo "Недопустимая длина логина";
     exit();
   } else if(mb_strlen($name)< 3 || mb_strlen($name)>50){
     echo "Недопустимая длина имени ";
     exit();
-  } else if(mb_strlen($pass)< 2 || mb_strlen($pass)>6){
-    echo "Недопустимая длина пароля ( от 2 до 6 символов) ";
+  } else if(mb_strlen($pass)< 3 || mb_strlen($pass)>30){
+    echo "Недопустимая длина пароля ( от 2 до 30 символов) ";
     exit();}
     else{
+      $pass = md5($pass."sadsd23k");
       $vars = ['log' =>$login, 'nam'=>$name,'pas'=>$pass];
       $this->model->getRegistr($vars);
     }
-      
-  $this->view->render('Регистрация');
 
+  }
+  public function autorizationAction(){
+    $login = filter_var(trim($_POST['login']),FILTER_SANITIZE_STRING);
+    $pass = filter_var(trim($_POST['pass']),FILTER_SANITIZE_STRING);
+
+
+    $pass = md5($pass."sadsd23k");
+      $vars = ['log' =>$login,'pas'=>$pass];
+      $this->model->getAutorization($vars);
+
+  }
+
+  public function exitAction(){
+    setcookie('result',$result[0]['name'],time() - 3600, "/");
+    header('Location: /');
   }
 }
 
--- application/views/account/login.php
+++ application/views/account/login.php
@@ -1,15 +1,23 @@
-
+<?php
+        if($_COOKIE["result"]==''):
+        ?>
 <div class="autorization">
-        
+
      <h3>Вход</h3>
-<form  action="/account/login" method = "post">
-   
+<form  action="/account/autorization" method = "post">
+
       <p>login</p>
-      <p> <input type="text" name="login"></p>
+      <p> <input type="text" name="login" id="login"></p>
       <p>пароль</p>
-      <p> <input type="text" name="password"></p>
+      <p> <input type="text" name="pass" id="pass"></p>
+
         <button type="submit" name="button"><b>Вход</b></button>
+
         <span>Нету аккаунта?</span><a href="/account/register">Зарегистрироваться</a>
 </form>
 
 </div>
+
+        <?php else: echo' <h3 style="padding-top:300px;">Вы авторизованы</h3>';
+
+endif;?>
