d7314e0825abc02198f7cbd1227ffb2f2e0b5c9a
merkuler <43567118+merkuler@users.noreply.github.com>
2020-01-15 08:17:59 +0300

    changes #3

--- application/views/layouts/general_page.php
+++ application/views/layouts/general_page.php
@@ -7,40 +7,41 @@
     <script src="/public/scripts/form.js"></script>
     <link rel="stylesheet" href="/public/css/style_general.css">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-</head>  
+</head>
 <body>
 
- 
-<div class="header">
-  <div class="container">
-          <div class="header__inner">
-          
-              <a href="/" class="header__logo">Хостос</a>
-              <nav class="nav__header">
-                  <a class="nav__link" href="">Хостинг</a>
-                  <a class="nav__link" href="">VPS</a>
-                  <a class="nav__link" href="">Домен</a>
-                  <a class="nav__link" href="">Цены</a>
-                  <a class="nav__link" href="/contacts">Контакты</a>
-                  <a class="nav__link" href="/account/login">Войти</a>
-              </nav>
-          </div>
-      
+
+  <div class="header">
+    <div class="container">
+            <div class="header__inner">
+                <a href="/" class="header__logo">Хостос</a>
+                <nav class="nav__header">
+                    <a class="nav__link" href="">Хостинг</a>
+                    <a class="nav__link" href="">VPS</a>
+                    <a class="nav__link" href="">Домен</a>
+                    <a class="nav__link" href="">Цены</a>
+                    <a class="nav__link" href="/contacts">Контакты</a>
+                    <a class="nav__link" href="/account/login">Войти</a>
+                </nav>
+                <a href="/account/exit">Выход</a>
+            </div>
+    </div>
+   
   </div>
-</div>
+
 
 <div class="background__header">
     <div class="container">
       <h1 class="intro__title">Лучший Хостинг</h1>
       <h3 class="intro__title_h3">Веб-хостинг,VPS,Домены и многое другое!</h3>
-  
+
       <a class="btn" href="#anchor" onclick="slowScroll('#anchor')">Узнать больше</a>
-    
+
     </div>
 </div>
 
 
-  
+
 
   <?php echo $content; ?>
 
